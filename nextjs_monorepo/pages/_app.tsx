cdd8db67fedeb60ed7e42d9ed6162036d48403f7
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-09-14 15:42:04 +0900

    refactor: 💡 GTM のスクリプト周りをリファクタ (#18)

--- nextjs_monorepo/pages/_app.tsx
+++ nextjs_monorepo/pages/_app.tsx
@@ -3,10 +3,12 @@ import type { AppProps } from 'next/app'
 import GoogleTagManager from '../components/GoogleTagManager';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-    <GoogleTagManager />
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <GoogleTagManager />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
