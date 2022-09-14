3ab6e46e814758dcc9be13bba955a106cdad33fb
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-09-14 16:16:07 +0900

    Next.js で GTM が動かないのでいったん全て Revert する (#20)
    * Revert "refactor: 💡 GTM のスクリプト周りをリファクタ (#18)"
    
    This reverts commit cdd8db67fedeb60ed7e42d9ed6162036d48403f7.
    
    * Revert "feat: 🎸 リンククリックを測定するためにサンプルのカード（リンク）を追加した (#17)"
    
    This reverts commit eada7f9c4286d026c8013ddf93f0a459ee0d9c7e.
    
    * Revert "feat: 🎸 Next.js に Google Tag Manager を導入した (#16)"
    
    This reverts commit 8e63305e16fee04d264490dfefff1d992907fa03.

--- nextjs_monorepo/pages/_app.tsx
+++ nextjs_monorepo/pages/_app.tsx
@@ -1,14 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import GoogleTagManager from '../components/GoogleTagManager';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GoogleTagManager />
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
